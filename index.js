80c9f2d954442f567587ad4f251db57864acc231
MDanielle007 <127674409+MDanielle007@users.noreply.github.com>
2023-04-07 20:02:11 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
 let prevScrollPos = window.pageYOffset;
 
-
 window.addEventListener("scroll", function() {
-    var header = document.querySelector(".header");
+    var header = document.querySelector("header");
     if (window.scrollY > 100) {
         header.classList.add("shadow"); // add the 'shadow' class
     } else {
         header.classList.remove("shadow"); // remove the 'shadow' class
     }
-
-    const currentScrollPos = window.pageYOffset;
-    
-    if (currentScrollPos < prevScrollPos || currentScrollPos === 0) {
-        header.classList.remove('header-hide');
-    } else {
-        header.classList.add('header-hide');
-    }
-    
-    prevScrollPos = currentScrollPos;
 });
+
+const toggleBtn = document.querySelector('.toggle_btn');
+const dropdownMenu = document.querySelector('.dropdown_menu');
+
+toggleBtn.onclick = function(){
+    dropdownMenu.classList.toggle('open');
+};
